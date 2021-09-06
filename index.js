5c1bb679869323fa0275c3bf9444e10cbb2ff93e
Manish <ymanish26798@gmail.com>
2021-09-06 14:55:44 +0530

    first commit exress server added

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+const helmet=require('helmet');
+const morgan=require('morgan');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,useUnifiedTopology:true
+},()=>console.log("Database Connected"));
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'))
+
+app.listen(8800,()=>{
+    console.log("Backend server is Running");
+})
